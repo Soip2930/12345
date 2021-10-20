1a5f19498cb74231abac7082ef7ab5da8e1dd307
Soip2930 <92176704+Soip2930@users.noreply.github.com>
2021-10-21 00:48:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from decimal import *
+from math import factorial
+
+
+def pi(n):
+pi = Decimal(0)
+k = 0
+while k < n:
+pi += (Decimal(-1) ** k) * (Decimal(factorial(6 * k)) / ((factorial(k) ** 3) * (factorial(3 * k))) * (
+13591409 + 545140134 * k) / (640320 ** (3 * k)))
+k += 1
+pi = pi * Decimal(10005).sqrt() / 4270934400
+pi = pi ** (-1)
+return pi
+
+n = int(input())
+getcontext().prec = n + 1
+print(pi(n))
